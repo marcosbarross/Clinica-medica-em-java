57b7ea455d05c1db8b80de86faa90b891bcd1e98
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-07-03 23:57:20 -0300

    Initial commit

---
+++ src/main/java/Escola.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+
+/**
+ *
+ * @author marco
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        Cadastro();
+    }
+}
